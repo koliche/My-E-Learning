e813c149bde7cea1c97e424ad9f5b0661aea44b7
M.koliche <66259634+koliche@users.noreply.github.com>
2022-03-27 13:34:16 +0000

    navbar & Fragment

--- app/src/main/java/com/example/mye_learning/MainActivity.java
+++ app/src/main/java/com/example/mye_learning/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.mye_learning;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mye_learning.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.homeNavbar:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.searcheNavbar:
+                    replaceFragment(new SearchFragment());
+                    break;
+                case R.id.saveNavbar:
+                    replaceFragment(new SaveFragment());
+                    break;
+                case R.id.settingsNavbar:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
